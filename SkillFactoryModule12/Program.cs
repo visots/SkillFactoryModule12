df146d6d82fd5466531e7c5da9d211bb1d0b98dc
Alexey <visotskiy.alexey@gmail.com>
2023-03-19 20:37:48 +0700

    Задания 12.1.3 и 12.1.4

--- SkillFactoryModule12/Program.cs
+++ SkillFactoryModule12/Program.cs
@@ -5,6 +5,34 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            BuildArray();
+            CheckAge();
+
+        }
+
+        static void BuildArray()
+        {
+            Console.WriteLine("Сколько элементов будет в массиве?");
+
+            int count = Int32.Parse(Console.ReadLine());
+            int [] array = new int[count];
+
+            for (int i = 0; i < count; i++)
+                array[i] = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Все элементы записаны");
+        }
+
+        static void CheckAge()
+        {
+            Console.WriteLine("Введите свой возраст");
+
+            int age = Int32.Parse(Console.ReadLine());
+
+            if (age > 13)
+                Console.WriteLine("Вы успешно зарегистрированы");
+            else
+                Console.WriteLine("Пользователи младше 14 лет не могут быть зарегистрированы");
         }
     }
 }
